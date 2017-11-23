4ad252c766c555fc18d8272fd70570b45c600868
Stuart Moore <stuart.moore@gmail.com>
2017-11-23 11:56:41 +0000

    Let's try the google provided module instead

--- nat-gateway.tf
+++ nat-gateway.tf
@@ -1,7 +1,32 @@
+/*
 module "terraform-gcp-natgateway" {
   source = "github.com/migs/terraform-gcp-natgateway"
   project = "${var.project}"
   region = "${var.region}"
   prefix = "${var.prefix}"
   ha = "${var.ha}"
+}
+*/
+
+module "nat-gateway-1" {
+  source = "GoogleCloudPlatform/nat-gateway/google"
+  region = "${var.region}"
+  network = "${var.network}"
+  zone = "${lookup(var.region_params["${var.region}"],zone1)}"
+}
+
+module "nat-gateway-2" {
+  count = "${var.ha ? 1 : 0}"
+  source = "GoogleCloudPlatform/nat-gateway/google"
+  region = "${var.region}"
+  network = "${var.network}"
+  zone = "${lookup(var.region_params["${var.region}"],zone2)}"
+}
+
+module "nat-gateway-3" {
+  count = "${var.ha ? 1 : 0}"
+  source = "GoogleCloudPlatform/nat-gateway/google"
+  region = "${var.region}"
+  network = "${var.network}"
+  zone = "${lookup(var.region_params["${var.region}"],zone3)}"
 }
