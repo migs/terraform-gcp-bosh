7392f6f56c77d118ea2d69817245095afd93b8f5
Stuart Moore <stuart.moore@gmail.com>
2017-11-23 11:59:19 +0000

    Let's try the google provided module instead

--- variables.tf
+++ variables.tf
@@ -2,4 +2,18 @@ variable "project" { }
 variable "region" { }
 variable "prefix" { }
 variable "ha" { }
-
+variable "network" { }
+variable "region_params" {
+  type = "map"
+  default = [
+    europe-west1 {
+      zone1 = "europe-west1-b"
+      zone2 = "europe-west1-c"
+      zone3 = "europe-west1-d"
+    }
+    europe-west2 {
+      zone1 = "europe-west2-a"
+      zone2 = "europe-west2-b"
+      zone3 = "europe-west2-c"
+    }
+}
