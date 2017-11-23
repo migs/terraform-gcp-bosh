546754d13b81da0d68c8740b9de6c8ef6afa117f
Stuart Moore <stuart.moore@gmail.com>
2017-11-23 11:11:08 +0000

    Fix prefix

--- nat-gateway.tf
+++ nat-gateway.tf
@@ -2,6 +2,6 @@ module "terraform-gcp-natgateway" {
   source = "github.com/migs/terraform-gcp-natgateway"
   project = "${var.project}"
   region = "${var.region}"
-  prefix = "${var.prefix}-"
+  prefix = "${var.prefix}"
   ha = "${var.ha}"
 }
