47f8ff35a242762925ce129852f5f2758d56006b
Stuart Moore <stuart.moore@gmail.com>
2017-11-23 12:05:42 +0000

    Back to custom scripts ...

--- variables.tf
+++ variables.tf
@@ -3,18 +3,3 @@ variable "region" { }
 variable "prefix" { default = "default" }
 variable "ha" { default = "false" }
 variable "network" { default = "default" }
-variable "region_params" {
-  type = "map"
-  default = {
-    europe-west1 {
-      zone1 = "europe-west1-b"
-      zone2 = "europe-west1-c"
-      zone3 = "europe-west1-d"
-    }
-    europe-west2 {
-      zone1 = "europe-west2-a"
-      zone2 = "europe-west2-b"
-      zone3 = "europe-west2-c"
-    }
-  }
-}
