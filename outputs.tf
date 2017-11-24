dcaf7b9b5019cd2284770b9b28c5eb8733e7c664
Stuart Moore <stuart.moore@gmail.com>
2017-11-24 14:13:31 +0000

    Add nat-gatway outputs

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,7 @@ output "nat-gateway-zone2-external_ip" {
 output "nat-gateway-zone3-external_ip" {
   value = "${module.terraform-gcp-natgateway.nat-gateway-zone3-external_ip}"
 }
+
+output "bosh-bastion-external_ip" {
+  value = "${google_compute_address.bosh-bastion-address.address}"
+}
