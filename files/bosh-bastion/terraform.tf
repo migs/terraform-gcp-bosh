3ab06da87159c00460253fe72bced339d2ae5f96
Stuart Moore <stuart.moore@gmail.com>
2017-11-24 13:54:57 +0000

    Fix files/bosh-bastion/terraform.tf

--- files/bosh-bastion/terraform.tf
+++ files/bosh-bastion/terraform.tf
@@ -4,22 +4,14 @@ data "terraform_remote_state" "%%PROJECT" {
     credentials = "%%SERVICE_ACCOUNT_ID-%%PROJECT.key.json"
     bucket = "%%PROJECT-terraform"
     project = "%%PROJECT"
-    path = "%%ENV.tfstate"
   }
 }
 
-output "db-primary-ip" {
-  value = "${data.terraform_remote_state.%%PROJECT.db-primary-ip}"
-}
-
-output "wordpress-password" {
-  value = "${data.terraform_remote_state.%%PROJECT.wordpress-password}"
-}
-
-output "env" {
-  value = "${data.terraform_remote_state.%%PROJECT.env}"
+output "workspace" {
+  value = "${data.terraform_remote_state.stuart-finkit.workspace}"
 }
 
 output "service_account_key" {
-  value = "${data.terraform_remote_state.%%PROJECT.automated.private_key}"
+  value = "${data.terraform_remote_state.stuart-finkit.service_account_key}"
+  sensitive = true
 }
