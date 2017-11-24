553504a222a918c9bacbdcba7cc74404673bedfd
Stuart Moore <stuart.moore@gmail.com>
2017-11-24 12:53:21 +0000

    Add service_account to bosh-bastion/terraform.tf

--- files/bosh-bastion/terraform.tf
+++ files/bosh-bastion/terraform.tf
@@ -1,6 +1,7 @@
 data "terraform_remote_state" "%%PROJECT" {
   backend = "gcs"
   config {
+    credentials = "%%SERVICE_ACCOUNT_ID-%%PROJECT.key.json"
     bucket = "%%PROJECT-terraform"
     project = "%%PROJECT"
     path = "%%ENV.tfstate"
