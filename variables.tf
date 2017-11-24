ee76996b75fbc8a0460e01e52f9e2fa28ae1b978
Stuart Moore <stuart.moore@gmail.com>
2017-11-24 12:55:44 +0000

    Add service_account to bosh-bastion/terraform.tf

--- variables.tf
+++ variables.tf
@@ -27,7 +27,7 @@ variable "home" { default = "/home/vagrant" }
 variable "control-cidr" { default = "10.0.0.0/24" }
 variable "ert-cidr" { default = "10.10.0.0/22" }
 variable "director-ip" { default = "10.0.0.6" }
-variable "service_account_email" { }
+variable "service_account" { }
 variable "service_account_key" { }
 variable "bosh-machine_type" { default = "f1-micro" }
 variable "ssh-privatekey" { default = "" }
