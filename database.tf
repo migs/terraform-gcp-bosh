09a2d766c3d38cab637de79d0b27c68604654652
Stuart Moore <stuart.moore@gmail.com>
2017-11-27 15:26:46 +0000

    Add terraform-gcp-database module for bosh database

---
+++ database.tf
@@ -0,0 +1,8 @@
+module "bosh-db" {
+  source "github.com/migs/terraform-gcp-database"
+  project = "${var.project}"
+  region = "${var.region}"
+  prefix = "${var.prefix}"
+  ha = "${var.ha}"
+  db-name = "bosh"
+}
