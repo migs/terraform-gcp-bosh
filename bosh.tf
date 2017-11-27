8819fe5aa19a7bd292abcc1b1ca2580f1bea585b
Stuart Moore <stuart.moore@gmail.com>
2017-11-27 12:47:35 +0000

    sed was modifying the wrong file - now fixed

--- bosh.tf
+++ bosh.tf
@@ -94,9 +94,9 @@ resource "google_compute_instance" "bosh-bastion" {
     inline = [
       "chmod +x ${var.home}/*.sh",
       "sed -i 's/%%PROJECT/${var.project}/' ${var.home}/terraform.tf",
-      "sed -i 's/%%ENV/${var.prefix}/' ${var.home}/terraform.tf",
       "sed -i 's/%%SERVICE_ACCOUNT_ID/${google_service_account.automated.account_id}/' ${var.home}/terraform.tf",
       "sed -i 's/%%ENV/${var.prefix}/' ${var.home}/cloud-config.yml",
+      "sed -i 's/%%ENV/${var.prefix}/' ${var.home}/create-bosh.sh",
     ]
     connection {
       user = "vagrant"
