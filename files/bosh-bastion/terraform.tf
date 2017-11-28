f6ed46e8677a2e1fb40bbe9d3f94dc694a9bf773
Stuart Moore <stuart.moore@gmail.com>
2017-11-28 14:20:45 +0000

    Add more outputs

--- files/bosh-bastion/terraform.tf
+++ files/bosh-bastion/terraform.tf
@@ -9,16 +9,31 @@ data "terraform_remote_state" "%%PROJECT" {
 
 output "bosh-db-bosh-password" {
   value = "${data.terraform_remote_state.%%PROJECT.bosh-db-bosh-password}"
+  sensitive = true
 }
 
 output "bosh-db-credhub-password" {
-  value = "${data.terraform_remote_state.%%PROJECT.bosh-db-bosh-password}"
+  value = "${data.terraform_remote_state.%%PROJECT.bosh-db-credhub-password}"
+  sensitive = true
 }
 
 output "bosh-db-uaa-password" {
-  value = "${data.terraform_remote_state.%%PROJECT.bosh-db-bosh-password}"
+  value = "${data.terraform_remote_state.%%PROJECT.bosh-db-uaa-password}"
+  sensitive = true
 }
 
 output "bosh-db-instance-ip" {
   value = "${data.terraform_remote_state.%%PROJECT.bosh-db-instance-ip}"
+}
+
+output "bosh-director-ip" {
+  value = "${data.terraform_remote_state.%%PROJECT.bosh-director-ip}"
+}
+
+output "bosh-control-cidr" {
+  value = "${data.terraform_remote_state.%%PROJECT.bosh-control-cidr}"
+}
+
+output "bosh-control-gw" {
+  value = "${data.terraform_remote_state.%%PROJECT.bosh-control-gw}"
 }
