0577693cffb8c3b109fe733b3339510a728df533
Stuart Moore <stuart.moore@gmail.com>
2017-11-28 11:13:48 +0000

    terraform-gcp-natgateway nolonger requires a prefix variable

--- nat-gateway.tf
+++ nat-gateway.tf
@@ -2,7 +2,6 @@ module "terraform-gcp-natgateway" {
   source = "github.com/migs/terraform-gcp-natgateway"
   project = "${var.project}"
   region = "${var.region}"
-  prefix = "${var.prefix}"
   zones = "${var.zones}"
   network = "${google_compute_network.bosh.name}"
   subnetwork = "${google_compute_subnetwork.control-subnet-1.name}"
