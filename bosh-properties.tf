81aa38b0997d0e8c7df5090bb3b4d18edf9c4870
Stuart Moore <stuart.moore@gmail.com>
2017-11-29 14:24:41 +0000

    Move more variables around in template

--- bosh-properties.tf
+++ bosh-properties.tf
@@ -5,18 +5,25 @@ export BOSH_DB_HOST=$${db-instance-ip}
 export BOSH_DB_PORT=$${db-port}
 export BOSH_DB_TYPE=$${db-type}
 export BOSH_DB_ADAPTER=$${db-adapter}
-export BOSH_DIRECTOR_IP=$${var.director-ip}
-export GCP_CONTROL_CIDR=$${var.control-cidr}
-export GCP_CONTROL_GW=$${var.control-gw}
-export GCP_ENV=$${var.prefix}
-export BOSH_DB_BOSH_PASSWORD=$${random_string.bosh-password.result}
-export BOSH_DB_CREDHUB_PASSWORD=$${random_string.bosh-credhub-password.result}
-export BOSH_DB_UAA_PASSWORD=$${random_string.bosh-uaa-password.result}
+export BOSH_DIRECTOR_IP=$${director-ip}
+export GCP_CONTROL_CIDR=$${control-cidr}
+export GCP_CONTROL_GW=$${control-gw}
+export GCP_ENV=$${prefix}
+export BOSH_DB_BOSH_PASSWORD=$${bosh-password}
+export BOSH_DB_CREDHUB_PASSWORD=$${bosh-credhub-password}
+export BOSH_DB_UAA_PASSWORD=$${bosh-uaa-password}
 EOF
   vars {
     db-instance-ip = "${module.bosh-db.db-instance-ip}"
     db-port = "${lookup(var.database_params["port"], var.db-version)}"
     db-type = "${lookup(var.database_params["type"], var.db-version)}"
     db-adapter = "${lookup(var.database_params["bosh-adapter"], var.db-version)}"
+    director-ip = "${var.director-ip}"
+    control-cidr = "${var.control-cidr}"
+    control-gw = "${var.control-gw}"
+    prefix = "${var.prefix}"
+    bosh-password = "${random_string.bosh-password.result}"
+    bosh-credhub-password = "${random_string.bosh-credhub-password.result}"
+    bosh-uaa-password = "${random_string.bosh-uaa-password.result}"
   }
 }
