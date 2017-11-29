240bdf78e861f964378a727e22abd8733bd0c895
Stuart Moore <stuart.moore@gmail.com>
2017-11-29 14:16:47 +0000

    Move more variables around in template

--- bosh-properties.tf
+++ bosh-properties.tf
@@ -2,9 +2,9 @@ data "template_file" "bosh-properties" {
   template = <<EOF
 #!/usr/bin/env bash
 export BOSH_DB_HOST=$${db-instance-ip}
-export BOSH_DB_PORT=$${lookup(var.database_params["port"], var.db-version)}
-export BOSH_DB_TYPE=$${lookup(var.database_params["type"], var.db-version)}
-export BOSH_DB_ADAPTER=$${lookup(var.database_params["bosh-adapter"], var.db-version)}
+export BOSH_DB_PORT=$${db-port)}
+export BOSH_DB_TYPE=$${db-type)}
+export BOSH_DB_ADAPTER=$${db-adapter)}
 export BOSH_DIRECTOR_IP=$${var.director-ip}
 export GCP_CONTROL_CIDR=$${var.control-cidr}
 export GCP_CONTROL_GW=$${var.control-gw}
@@ -15,5 +15,8 @@ export BOSH_DB_UAA_PASSWORD=$${random_string.bosh-uaa-password.result}
 EOF
   vars {
     db-instance-ip = "${module.bosh-db.db-instance-ip}"
+    db-port = "${lookup(var.database_params["port"], var.db-version)}"
+    db-type = "${lookup(var.database_params["type"], var.db-version)}"
+    db-adapter = "${lookup(var.database_params["adapter"], var.db-version)}"
   }
 }
