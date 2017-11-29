7e6758d991a8db8d523dadc913534b70117b35bc
Stuart Moore <stuart.moore@gmail.com>
2017-11-29 14:08:19 +0000

    Output cleanup, move module variable to vars section in template

--- bosh-properties.tf
+++ bosh-properties.tf
@@ -1,7 +1,7 @@
 data "template_file" "bosh-properties" {
   template = <<EOF
 #!/usr/bin/env bash
-export BOSH_DB_HOST=$${module.bosh-db.db-instance-ip}
+export BOSH_DB_HOST=$${db-instance-ip}
 export BOSH_DB_PORT=$${lookup(var.database_params["port"], var.db-version)}
 export BOSH_DB_TYPE=$${lookup(var.database_params["type"], var.db-version)}
 export BOSH_DB_ADAPTER=$${lookup(var.database_params["bosh-adapter"], var.db-version)}
@@ -13,4 +13,7 @@ export BOSH_DB_BOSH_PASSWORD=$${random_string.bosh-password.result}
 export BOSH_DB_CREDHUB_PASSWORD=$${random_string.bosh-credhub-password.result}
 export BOSH_DB_UAA_PASSWORD=$${random_string.bosh-uaa-password.result}
 EOF
+  vars {
+    db-instance-ip = "${module.bosh-db.db-instance-ip}"
+  }
 }
--- outputs.tf
+++ outputs.tf
@@ -6,21 +6,6 @@ output "bosh-bastion-public-ip" {
   value = "${google_compute_address.bosh-bastion-address.address}"
 }
 
-output "bosh-db-bosh-password" {
-  value = "${random_string.bosh-password.result}"
-  sensitive = true
-}
-
-output "bosh-db-uaa-password" {
-  value = "${random_string.bosh-uaa-password.result}"
-  sensitive = true
-}
-
-output "bosh-db-credhub-password" {
-  value = "${random_string.bosh-credhub-password.result}"
-  sensitive = true
-}
-
 output "nat-gateway-ips" {
   value = "${module.terraform-gcp-natgateway.nat-gateway-ips}"
 }
