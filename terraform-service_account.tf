18febc663f6bc1c899363c7c0d3231a3b0e1281e
Stuart Moore <stuart.moore@gmail.com>
2018-01-16 13:38:22 +0000

    Removing service account generation

--- terraform-service_account.tf
+++ terraform-service_account.tf
@@ -1,23 +1,3 @@
-resource "google_service_account" "automated" {
-  project = "${var.project}"
-  account_id = "${var.prefix}-${var.service_account_name}"
-  display_name = "Automated Service Account"
-}
-
-resource "google_project_iam_policy" "automated" {
-  project = "${var.project}"
-  policy_data = "${data.google_iam_policy.automated.policy_data}"
-}
-
-data "google_iam_policy" "automated" {
-  binding {
-    role = "${var.service_account_role}"
-     members = [
-    "serviceAccount:${google_service_account.automated.email}",
-    ]
-  }
-}
-
 resource "google_service_account_key" "automated" {
-  service_account_id = "${google_service_account.automated.id}"
+  service_account_id = "${var.service_account_name}"
 }
